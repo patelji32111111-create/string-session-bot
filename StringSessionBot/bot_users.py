a16f3c14d95700f2f89236c5e076dfbb7ff9920d
patelji32111111-create <patelji32111111@gmail.com>
2026-08-14 21:05:56 +0530

    Update bot_users.py

--- StringSessionBot/bot_users.py
+++ StringSessionBot/bot_users.py
@@ -15,7 +15,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(6174058850) & filters.command("stats"))
+@Client.on_message(filters.user(7383472822) & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     await msg.reply(f"ᴛᴏᴛᴀʟ ᴜsᴇʀs : {users}", quote=True)
